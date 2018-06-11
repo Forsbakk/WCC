3a35e581b725156f39ddc99aa1ffc556dc1bb688
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-06-11 08:54:28 +0200

    grps

--- Create-Groups.ps1
+++ Create-Groups.ps1
@@ -11,7 +11,7 @@ function Get-Team {
 function Get-Groups {
     [cmdletbinding()]
     
-    $Group = "a","b","c","d", "e", "f", "g", "h"
+    $Group = "a", "b", "c", "d", "e", "f", "g", "h"
     
     foreach ($i in $Group) {
         $grp = $Data | Select-Object -ExpandProperty groups | Select-Object -ExpandProperty "$i" | Select-Object -ExpandProperty matches
@@ -25,13 +25,15 @@ function Get-Groups {
             }
         }
         $Groups = @{
-                Name = "Gruppe $($i.ToUpper())"
-                Teams = $ids | ForEach-Object {
-                    New-Object psobject -Property @{ ID = $_ ; Name = Get-Team -ID $_ }   
-                }
+            Name  = "Gruppe $($i.ToUpper())"
+            Teams = $ids | ForEach-Object {
+                New-Object psobject -Property @{ ID = $_; Name = Get-Team -ID $_; Played = [int]0; Win = [int]0; Draw = [int]0; Loss = [int]0; Points = [int]0; GoalIn = [int]0; GoalOut = [int]0; GoalDiff = [int]0 }
             }
+        }
         New-Object psobject -Property $Groups
     }
 }
 
+
 $Groups = Get-Groups
+$Groups | ConvertTo-Json -Depth 3 -Compress | Out-File groups.json
